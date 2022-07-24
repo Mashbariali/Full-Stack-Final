f7d7ee0a02d4a6285fcba4c9937d5a692d45ef11
Abdulaziz Altariqi <AbdulazizAlturaiqi@gmail.com>
2022-07-24 14:44:08 +0300

    edit app.js by Abdulaziz

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -4,7 +4,6 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './components/Home';
 import ListOrders from './components/ListOrders';
-import Orders from './components/Orders';
 import Join from './components/Join';
 import Login from './components/Login'
 import Register from './components/Register';
@@ -22,7 +21,6 @@ function App() {
 
             <Route path='/'                   element={< Home />}></Route>
             <Route path='/list-orders'              element={< ListOrders />}></Route>
-            <Route path='/orders'              element={< Orders />}></Route>
             <Route path='/Login'                   element={< Login />}></Route>
             <Route path='/Register'              element={< Register />}></Route>
             <Route path='/Join'                   element={< Join />}></Route>
