45bc3f9be2330d99069d124cc8513ecc93d145ce
ali <mashbariali@gmail.com>
2022-07-24 14:36:31 +0300

    BackEnd-3 Commit

--- Back-End/DeliveryProject/DeliveryApp/urls.py
+++ Back-End/DeliveryProject/DeliveryApp/urls.py
@@ -5,16 +5,16 @@ from . import views
 urlpatterns=[
     path("add_Delegate", views.add_Delegate, name="add_Delegate"),
     path("list_Delegate", views.list_Delegate, name="list_Delegate"),
-    path("update_Delegate", views.update_Delegate, name="update_Delegate"),
-    path("delete_Delegate", views.delete_Delegate, name="delete_Delegate"),
+    path("update_Delegate/<Delegate_id>", views.update_Delegate, name="update_Delegate"),
+    path("delete_Delegate/<Delegate_id>", views.delete_Delegate, name="delete_Delegate"),
     path("add_Order", views.add_Order, name="add_Order"),
     path("list_Orders", views.list_Orders, name="list_Orders"),
-    path("update_Order", views.update_Order, name="update_Order"),
-    path("delete_Order", views.delete_Order, name="delete_Order"),
+    path("update_Order/<Order_id>", views.update_Order, name="update_Order"),
+    path("delete_Order/<Order_id>", views.delete_Order, name="delete_Order"),
     path("add_AppRating", views.add_AppRating, name="add_AppRating"),
-    path("delete_AppRating", views.delete_AppRating, name="delete_AppRating"),
+    path("delete_AppRating/<AppRating_id>", views.delete_AppRating, name="delete_AppRating"),
     path("add_DelegateRating", views.add_DelegateRating, name="add_DelegateRating"),
-    path("delete_DelegateRating", views.delete_DelegateRating, name="delete_DelegateRating"),
+    path("delete_DelegateRating/<DelegateRating_id>", views.delete_DelegateRating, name="delete_DelegateRating"),
 
 ]
 
